8b0d0b9d448fd6e1cf0a8a7419df3800fd104bf2
Aminjoni Abdullozoda <amin030296@gmail.com>
2019-07-03 11:18:22 +0500

    coredata init

---
+++ Coredata-course/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coredata-course
+//
+//  Created by Aminjoni Abdullozoda on 7/3/19.
+//  Copyright © 2019 Aminjoni Abdullozoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
